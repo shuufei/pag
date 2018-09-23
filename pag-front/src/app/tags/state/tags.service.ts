8fbee4ad938ae2e8ca07ce14e0757910cd9f4079
shuufei <digital.shuufei@gmail.com>
2018-09-24 01:06:25 +0900

    Fixed generate navtag process.

--- pag-front/src/app/tags/state/tags.service.ts
+++ pag-front/src/app/tags/state/tags.service.ts
@@ -19,7 +19,6 @@ export class TagsService {
   }
 
   setTags(navTags: NavTag[]): void {
-    navTags = this.getTags();
     this.tagsStore.setState(state => ({ ...state, navTags }));
   }
 
@@ -37,21 +36,7 @@ export class TagsService {
       { tag: 'Design System', count: 20, selected: false, disable: false },
       { tag: 'Service Worker', count: 11, selected: false, disable: false },
       { tag: 'Typogpraphy', count: 5, selected: false, disable: false },
-      { tag: '1Long long to long tag name super long', count: 1, selected: false, disable: false },
-      { tag: '1Development', count: 42, selected: false, disable: false },
-      { tag: '1Design', count: 40, selected: false, disable: false },
-      { tag: '1Angular', count: 28, selected: false, disable: false },
-      { tag: '1UI', count: 27, selected: false, disable: false },
-      { tag: '1Design System', count: 20, selected: false, disable: false },
-      { tag: '1Service Worker', count: 11, selected: false, disable: false },
-      { tag: '1Typogpraphy', count: 5, selected: false, disable: false },
-      { tag: '2Development', count: 42, selected: false, disable: false },
-      { tag: '2Design', count: 40, selected: false, disable: false },
-      { tag: '2Angular', count: 28, selected: false, disable: false },
-      { tag: '2UI', count: 27, selected: false, disable: false },
-      { tag: '2Design System', count: 20, selected: false, disable: false },
-      { tag: '2Service Worker', count: 11, selected: false, disable: false },
-      { tag: '2Typogpraphy', count: 5, selected: false, disable: false }
+      { tag: '1Long long to long tag name super long', count: 1, selected: false, disable: false }
     ];
   }
 
