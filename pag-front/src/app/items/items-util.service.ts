02bf358373de47cf279b8c040bc61ececa195aec
shuufei <digital.shuufei@gmail.com>
2018-09-26 22:43:34 +0900

    Fixed item background.

--- pag-front/src/app/items/items-util.service.ts
+++ pag-front/src/app/items/items-util.service.ts
@@ -104,6 +104,104 @@ export class ItemsUtilService {
           { label: 'Angular', active: false }
         ],
         star: false
+      },
+      {
+        id: '1',
+        title: 'Angularの状態管理設計',
+        comment: 'RxJSやAkitaなどの活用法',
+        thumbUrl: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Development', active: false },
+          { label: 'Angular', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '2',
+        title: 'Design Sysmte Management',
+        comment: 'Design System管理法',
+        thumbUrl: 'https://cdn-static-1.medium.com/_/fp/icons/monogram-mask.KPLCSFEZviQN0jQ7veN2RQ.svg',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design System', active: false },
+          { label: 'Design', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '3',
+        title: 'Design Ecosystem In Lagos',
+        comment: 'Design Ecosystem by Phase',
+        thumbUrl: 'https://phase.com/wp-content/uploads/2018/08/lagos_featured-1.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design', active: false },
+          { label: 'Design Tool', active: false },
+          { label: 'Phase', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '4',
+        title: 'Atomic Design by Angular',
+        comment: 'Atomic DesignをAngularで実践した例',
+        thumbUrl: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design', active: false },
+          { label: 'Angular', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '1',
+        title: 'Angularの状態管理設計',
+        comment: 'RxJSやAkitaなどの活用法',
+        thumbUrl: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Development', active: false },
+          { label: 'Angular', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '2',
+        title: 'Design Sysmte Management',
+        comment: 'Design System管理法',
+        thumbUrl: 'https://cdn-static-1.medium.com/_/fp/icons/monogram-mask.KPLCSFEZviQN0jQ7veN2RQ.svg',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design System', active: false },
+          { label: 'Design', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '3',
+        title: 'Design Ecosystem In Lagos',
+        comment: 'Design Ecosystem by Phase',
+        thumbUrl: 'https://phase.com/wp-content/uploads/2018/08/lagos_featured-1.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design', active: false },
+          { label: 'Design Tool', active: false },
+          { label: 'Phase', active: false }
+        ],
+        star: false
+      },
+      {
+        id: '4',
+        title: 'Atomic Design by Angular',
+        comment: 'Atomic DesignをAngularで実践した例',
+        thumbUrl: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        url: 'https://cdn.qiita.com/assets/qiita-fb-2887e7b4aad86fd8c25cea84846f2236.png',
+        tags: [
+          { label: 'Design', active: false },
+          { label: 'Angular', active: false }
+        ],
+        star: false
       }
     ];
   }
